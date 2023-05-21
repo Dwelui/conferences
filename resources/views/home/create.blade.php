6d9902844867dc117b4234cbb51e65305cc94626
Dwelui <mantas.motuzas41@gmail.com>
2023-05-21 19:15:56 +0300

    add back

--- resources/views/home/create.blade.php
+++ resources/views/home/create.blade.php
@@ -5,14 +5,16 @@
 @section('content')
   <form action="{{ route('submit') }}" method="POST">
     @csrf
-    <label for="name"><p>Name</p></label>
-    <input type="text" name="name" id="name">
-    <label for="description"><p>Description</p></label>
-    <textarea name="description" id="description" cols="30" rows="10"></textarea>
-    <label for="date"><p>Date</p></label>
-    <input type="date" name="date" id="date">
-    <label for="location"><p>Location</p></label>
-    <input type="text" name="location" id="location">
+    <label for="name"><p>Name</p></label> <br>
+    <input type="text" name="name" id="name"> <br> <br>
+    <label for="description"><p>Description</p></label> <br>
+    <textarea name="description" id="description" cols="30" rows="10"></textarea> <br> <br>
+    <label for="date"><p>Date</p></label> <br>
+    <input type="date" name="date" id="date"> <br> <br>
+    <label for="location"><p>Location</p></label> <br>
+    <input type="text" name="location" id="location"> <br> <br>
     <input type="submit" value="Add">
   </form>
+  <br>
+  <a href="{{ route('home.index') }}">Back</a>
 @endsection
